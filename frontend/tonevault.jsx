d5d64583d33b3087dda477fb6c20002e1cc52c40
siascone <spencer.iascone@gmail.com>
2021-09-20 22:01:13 -0600

    Setup base application and react functionality

---
+++ frontend/tonevault.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener('DOMContentLoaded', () => {
+    const root = document.querySelector('#root');
+    ReactDOM.render(<h1>Tonevault is coming soon...</h1>, root)
+});
