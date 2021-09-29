b2ce8726b01f5b494359920d0378c69c7bb76ce4
siascone <spencer.iascone@gmail.com>
2021-09-29 17:25:35 -0600

    Complete initial splash styling/layout

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -9,7 +9,15 @@ class Splash extends React.Component {
         return (
             <div className='splash-main'>
                 <div className='splash-carousel'>
-                    Carousel
+                    <h1>What's next in music is first on ToneVault</h1>
+                    <br />
+                    <p>
+                        Upload your first track and begin your journey. ToneVault gives
+                        you space to create, find your fans, and connect with other
+                        artists.
+                    </p>
+                    <br />
+                    <button className='splash-upload'>Start uploading today</button>
                 </div>
 
                 <div className='splash-search-upload'>
@@ -27,30 +35,79 @@ class Splash extends React.Component {
                 </div>
 
                 <div className='splash-spotlight'>
-                    <p>Here what's trending for free in the ToneVault community</p>
-                    <div>
-                        Collection of trending songs and artists
+                    <h2>Here what's trending for free in the ToneVault community</h2>
+                    <div className='trending-songs'>
+                        <div className='trend-set1'>
+                            <div className='trending-song'>
+                                <h3>Awesome Song 1</h3>
+                                <p>Awesome Artist</p>
+                            </div>
+                            <div className='trending-song'>
+                                <h3>Awesome Song 2</h3>
+                                <p>Awesome Artist</p>
+                            </div>
+                            <div className='trending-song'>
+                                <h3>Awesome Song 3</h3>
+                                <p>Awesome Artist</p>
+                            </div>
+                            <div className='trending-song'>
+                                <h3>Awesome Song 4</h3>
+                                <p>Awesome Artist</p>
+                            </div>
+                            <div className='trending-song'>
+                                <h3>Awesome Song 5</h3>
+                                <p>Awesome Artist</p>
+                            </div>
+                            <div className='trending-song'>
+                                <h3>Awesome Song 6</h3>
+                                <p>Awesome Artist</p>
+                            </div>
+                        </div>
+                        <div className='trend-set2'>
+                            <div className='trending-song'>
+                                <h3>Awesome Song 7</h3>
+                                <p>Awesome Artist</p>
+                            </div>
+                            <div className='trending-song'>
+                                <h3>Awesome Song 8</h3>
+                                <p>Awesome Artist</p>
+                            </div>
+                            <div className='trending-song'>
+                                <h3>Awesome Song 9</h3>
+                                <p>Awesome Artist</p>
+                            </div>
+                            <div className='trending-song'>
+                                <h3>Awesome Song 10</h3>
+                                <p>Awesome Artist</p>
+                            </div>
+                            <div className='trending-song'>
+                                <h3>Awesome Song 11</h3>
+                                <p>Awesome Artist</p>
+                            </div>
+                            <div className='trending-song'>
+                                <h3>Awesome Song 12</h3>
+                                <p>Awesome Artist</p>
+                            </div>
+                        </div>
                     </div>
                     <button className='explore'>Explore trending playlists</button>
                 </div>
 
                 <div className='splash-creators-cta'>
-                    <div>
-                        <h1>Calling all creators</h1>
-                        <p>Get on ToneVault to connect with fans, share your sounds, 
-                           and grow your audience. What are you waiting for?
-                        </p>
-                        <button className='creators-cta-button'>Find out more</button>
-                    </div>
+                    <h1>Calling all creators</h1>
+                    <p>Get on ToneVault to connect with fans, share your sounds, 
+                        and grow your audience. What are you waiting for?
+                    </p>
+                    <button className='creators-cta-button'>Find out more</button>
                 </div>
 
                 <div className='splash-thankyou'>
                     <h1>Thanks for listening. Now join in.</h1>
-                    <p>Save tracks, follow artists and build playlists. All for free.</p>
+                    <h2>Save tracks, follow artists and build playlists. All for free.</h2>
                     <button className='signup-button'>Create account</button>
-                    <div>
+                    <div className='splash-signup'>
                         <p>Already have an account?</p>
-                        <button className='login-button'>Sign in</button>
+                        <button className='splash-login-button'>Sign in</button>
                     </div>
                 </div>
 
