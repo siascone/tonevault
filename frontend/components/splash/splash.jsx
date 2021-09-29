685ca6dbe9949549af2a2354162381e81f20ba58
siascone <spencer.iascone@gmail.com>
2021-09-28 18:39:51 -0600

    Begin splash styling

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -8,7 +8,54 @@ class Splash extends React.Component {
     render() {
         return (
             <div className='splash-main'>
-                <h1>A super cool splash page on it's way...</h1>
+                <div className='splash-carousel'>
+                    Carousel
+                </div>
+
+                <div className='splash-search-upload'>
+                    <div>
+                        <label htmlFor="splash-search"></label>
+                        <input
+                            id='splash-search'
+                            type="text" 
+                            placeholder='Search for artists, bands, tracks, podcasts' 
+                        />
+                    </div>
+                    <p>or</p>
+                    <button className='upload'>Upload your own</button>
+                </div>
+
+                <div className='splash-spotlight'>
+                    <p>Here what's trending for free in the ToneVault community</p>
+                    <div>
+                        Collection of trending songs and artists
+                    </div>
+                    <button className='explore'>Explore trending playlists</button>
+                </div>
+
+                <div className='splash-creators-cta'>
+                    <div>
+                        <h1>Calling all creators</h1>
+                        <p>Get on ToneVault to connect with fans, share your sounds, 
+                           and grow your audience. What are you waiting for?
+                        </p>
+                        <button className='creators-cta-button'>Find out more</button>
+                    </div>
+                </div>
+
+                <div className='splash-thankyou'>
+                    <h1>Thanks for listening. Now join in.</h1>
+                    <p>Save tracks, follow artists and build playlists. All for free.</p>
+                    <button className='signup-button'>Create account</button>
+                    <div>
+                        <p>Already have an account?</p>
+                        <button className='login-button'>Sign in</button>
+                    </div>
+                </div>
+
+                <div className='spalsh-footer'>
+                    Footer stuff here
+                </div>
             </div>
         )
     }
